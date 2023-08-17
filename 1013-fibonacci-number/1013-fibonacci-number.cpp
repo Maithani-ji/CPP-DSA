6017bd0523cf170e5a4c4a56d8de12a52b4554fc
Tushar Maithani <maithaniji.official@gmail.com>
2023-08-17 23:19:16 +0530

    Time: 2 ms (71.72%), Space: 6 MB (24.65%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        int a=0, b=1, c=1;
+
+        for(int i=0;i<n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+            
+        }
+            return a;
+        
+    }
+};
