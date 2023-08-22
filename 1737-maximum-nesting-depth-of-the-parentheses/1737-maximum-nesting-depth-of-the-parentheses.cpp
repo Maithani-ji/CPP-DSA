250f45c292ee0e9c449a4e5697f5d2ee27080c16
Tushar Maithani <maithaniji.official@gmail.com>
2023-08-22 23:58:37 +0530

    Time: 0 ms (100%), Space: 6.3 MB (13.7%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,19 +1,39 @@
-class Solution {
+class Solution {//using without any extra space 
 public:
     int maxDepth(string s) {
+        int currentDepth = 0; // Initialize the current depth to 0
+        int maxDepth = 0; // Initialize the maximum depth to 0
 
-        stack<char> st; // Create a stack to keep track of opening parentheses
-        int ans = 0; // Initialize the answer to a very small value
-
-        for (auto c : s) {
+        for (char c : s) {
             if (c == '(') { // If an opening parenthesis is encountered
-                st.push(c); // Push onto the stack
-                ans = max(ans,(int)(st.size())); // Update max depth
+                currentDepth++; // Increment the current depth
+                maxDepth = max(maxDepth, currentDepth); // Update max depth if necessary
             } else if (c == ')') { // If a closing parenthesis is encountered
-                st.pop(); // Pop from the stack (closing a nested level)
+                currentDepth--; // Decrement the current depth (closing a nested level)
             }
         }
 
-        return ans; // Return the maximum depth of nested parentheses
+        return maxDepth; // Return the maximum depth of nested parentheses
     }
 };
+
+ //Using Stack 
+ // class Solution {
+// public:
+//     int maxDepth(string s) {
+
+//         stack<char> st; // Create a stack to keep track of opening parentheses
+//         int ans = 0; // Initialize the answer to a very small value
+
+//         for (auto c : s) {
+//             if (c == '(') { // If an opening parenthesis is encountered
+//                 st.push(c); // Push onto the stack
+//                 ans = max(ans,(int)(st.size())); // Update max depth
+//             } else if (c == ')') { // If a closing parenthesis is encountered
+//                 st.pop(); // Pop from the stack (closing a nested level)
+//             }
+//         }
+
+//         return ans; // Return the maximum depth of nested parentheses
+//     }
+// };
