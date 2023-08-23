f80d1b22da7c3e3c83c7f2bca392000b3c974279
Tushar Maithani <maithaniji.official@gmail.com>
2023-08-23 15:24:12 +0530

    Time: 21 ms (67.98%), Space: 16.3 MB (58.32%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,33 +1,64 @@
-class MinStack {
+// class MinStack {  // USING TWO STACk
 
-    stack<int> mn, st; // Two stacks: one for maintaining minimum values and one for the main stack.
+//     stack<int> mn, st; // Two stacks: one for maintaining minimum values and one for the main stack.
+// public:
+//     MinStack() {
+//         // Constructor, no explicit initialization needed.
+//     }
+    
+//     void push(int val) {
+//         if (mn.empty())
+//             mn.push(val); // Initialize the minimum stack with the first element.
+//         else
+//             mn.push(min(val, mn.top())); // Push the current minimum between val and the top of mn.
+      
+//         st.push(val); // Push the value onto the main stack.
+//     }
+    
+//     void pop() {
+//         st.pop(); // Pop the value from the main stack.
+//         mn.pop(); // Pop the corresponding minimum value from the minimum stack.
+//     }  
+    
+//     int top() {
+//         return st.top(); // Return the top value of the main stack.
+//     }
+    
+//     int getMin() {
+//         return mn.top(); // Return the top value of the minimum stack (the current minimum).
+//     }
+// };
+
+class MinStack { // USING SINGLE STACK with PAIR values
+
+    stack<pair<int, int>> st; // A single stack with value and minimum value pairs.
+
 public:
+
     MinStack() {
         // Constructor, no explicit initialization needed.
     }
     
     void push(int val) {
-        if (mn.empty())
-            mn.push(val); // Initialize the minimum stack with the first element.
+        if (st.empty())
+            st.push({val, val}); // Initialize with the first element as both value and minimum.
         else
-            mn.push(min(val, mn.top())); // Push the current minimum between val and the top of mn.
-      
-        st.push(val); // Push the value onto the main stack.
+            st.push({val, min(val, st.top().second)}); // Push value and updated minimum.
     }
     
     void pop() {
-        st.pop(); // Pop the value from the main stack.
-        mn.pop(); // Pop the corresponding minimum value from the minimum stack.
+        st.pop(); // Pop the top value from the stack.
     }  
     
     int top() {
-        return st.top(); // Return the top value of the main stack.
+        return st.top().first; // Return the value of the top element.
     }
     
     int getMin() {
-        return mn.top(); // Return the top value of the minimum stack (the current minimum).
+        return st.top().second; // Return the minimum value of the top element.
     }
 };
+
 
 /**
  * Your MinStack object will be instantiated and called as such:
