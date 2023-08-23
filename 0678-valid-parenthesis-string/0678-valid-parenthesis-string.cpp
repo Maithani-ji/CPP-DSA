c8052a30f68d4b4f3650585f33eba6013c20504c
Tushar Maithani <maithaniji.official@gmail.com>
2023-08-24 00:28:21 +0530

    Time: 2 ms (49.29%), Space: 6.1 MB (59.35%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,33 +1,61 @@
 class Solution {
 public:
-    bool checkValidString(string s) {
-        stack<char> st, ast;  // Create two stacks: st for '(' indices, ast for '*' indices
+ bool checkValidString(string s) {
+    int minOpen = 0;   // Keep track of minimum unmatched '(' count
+    int maxOpen = 0;   // Keep track of maximum unmatched '(' count
 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(') {
-                st.push(i);  // Push index of '(' onto the stack
-            } else if (s[i] == '*') {
-                ast.push(i);  // Push index of '*' onto the stack
-            } else {  // This handles the case of encountering ')'
-                if (!st.empty()) {
-                    st.pop();  // If there's a matching '(', pop it from the stack
-                } else if (!ast.empty()) {
-                    ast.pop();  // If there's no matching '(', pop a '*' from the stack
-                } else {
-                    return false;  // Unmatched ')' without '(' or '*' to balance
-                }
-            }
+    for (char c : s) {
+        if (c == '(') {
+            minOpen++;    // Treat '*' as '(' for minimum unmatched count
+            maxOpen++;    // Treat '*' as ')' for maximum unmatched count
+        } else if (c == ')') {
+            minOpen = max(0, minOpen - 1);  // Treat '*' as '(' for minimum unmatched count
+            maxOpen--;    // Treat '*' as ')' for maximum unmatched count
+        } else {  // '*'
+            minOpen = max(0, minOpen - 1);  // Treat '*' as '(' for minimum unmatched count
+            maxOpen++;    // Treat '*' as ')' for maximum unmatched count
         }
-        
-        while (!st.empty()) {
-            if (!ast.empty() && st.top() < ast.top()) {
-                st.pop();  // Match '*' with '('
-                ast.pop();
-            } else {
-                return false;  // Unmatched '(' without '*' to balance
-            }
+
+        if (maxOpen < 0) {
+            return false;  // More ')' than '(' and '*' at this point, invalid
         }
-        
-        return true;  // All parentheses and '*' matched and balanced
     }
+
+    return minOpen == 0;  // If all '(' are matched, the string is valid
+}
 };
+
+
+// class Solution { // Using STACK T-n S-n
+// public:
+//     bool checkValidString(string s) {
+//         stack<char> st, ast;  // Create two stacks: st for '(' indices, ast for '*' indices
+
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] == '(') {
+//                 st.push(i);  // Push index of '(' onto the stack
+//             } else if (s[i] == '*') {
+//                 ast.push(i);  // Push index of '*' onto the stack
+//             } else {  // This handles the case of encountering ')'
+//                 if (!st.empty()) {
+//                     st.pop();  // If there's a matching '(', pop it from the stack
+//                 } else if (!ast.empty()) {
+//                     ast.pop();  // If there's no matching '(', pop a '*' from the stack
+//                 } else {
+//                     return false;  // Unmatched ')' without '(' or '*' to balance
+//                 }
+//             }
+//         }
+        
+//         while (!st.empty()) {
+//             if (!ast.empty() && st.top() < ast.top()) {
+//                 st.pop();  // Match '*' with '('
+//                 ast.pop();
+//             } else {
+//                 return false;  // Unmatched '(' without '*' to balance
+//             }
+//         }
+        
+//         return true;  // All parentheses and '*' matched and balanced
+//     }
+// };
