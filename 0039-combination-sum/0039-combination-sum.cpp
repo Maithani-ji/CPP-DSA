814b4af0798372df5299723695fd55770e103c7e
Tushar Maithani <maithaniji.official@gmail.com>
2023-09-09 23:51:10 +0530

    Time: 165 ms (5.04%), Space: 114.5 MB (5.08%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,40 @@
+
+class Solution {//using generate all subset ALGO WITH MODIFICATION like-> sum , target, and making subset of repetitive element , and moving forward when found val sum greater  
+
+                //BRUTE FORCE // not DYNAMIC PROGRAMMING used
+
+public:
+    vector<vector<int>> ans;  // Initialize a vector to store the combinations that sum up to the target.
+
+    // Recursive function to generate combinations.
+    void generate(vector<int> a, vector<int> cand, int n, int i, int sum, int t) {
+        if (i == cand.size()) {
+            return;  // Base case: If we've processed all elements in the candidates, return.
+        }
+        if (sum >= t) {
+            if (sum == t) {
+                ans.push_back(a);  // If the current sum matches the target, add the combination to 'ans'.
+            }
+            return;  // Return regardless of whether sum matches the target or not.
+        }
+
+        a.push_back(cand[i]);  // Include the current candidate element in the combination.
+        sum += cand[i];        // Update the current sum.
+
+        // Recursive call to include the current candidate element again (since it can be used multiple times).
+        generate(a, cand, n, i, sum, t);// not using i+1, since i'th pos element can be used multiple times.
+
+        a.pop_back();  // Remove the last element from the combination.
+        sum -= cand[i];  // Update the sum after removing the last element.
+
+        // Recursive call to move to the next candidate element (i+1) and continue generating combinations.
+        generate(a, cand, n, i + 1, sum, t);//i+1 used since , if prev pos element sum got >target, then remove it and move forward
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& cand, int target) {
+        vector<int> a;  // Initialize an empty combination.
+        generate(a, cand, cand.size(), 0, 0, target);  // Start generating combinations.
+
+        return ans;  // Return the list of combinations that sum up to the target.
+    }
+};
