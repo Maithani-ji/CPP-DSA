975cf62660cc9332ebe7275e483dc1d0fba9031f
Tushar Maithani <maithaniji.official@gmail.com>
2023-09-11 15:18:09 +0530

    Time: 3 ms (63.85%), Space: 17.6 MB (7.08%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,30 +1,30 @@
-class Solution {
+class Solution {// same as combination sum 2 algo without sum // recursion // brute
 public:
-        vector<vector<int>>ans;
+    vector<vector<int>> ans; // Vector to store the resulting subsets
 
-        void generate(int i,vector<int>nums,vector<int>v)
-        {
-            if(i==nums.size())
-            {   
-                ans.push_back(v);
-                return;
-            }
-
-            v.push_back(nums[i]);
-            generate(i+1,nums,v);
-            while( i+1 < nums.size() && nums[i]==nums[i+1])
-            {
-                i++;
-            }
-            v.pop_back();
-            generate(i+1,nums,v);
+    // Recursive function to generate subsets
+    void generate(int i, vector<int> nums, vector<int> v) {
+        if (i == nums.size()) {
+            ans.push_back(v); // If we've processed all elements, add the current subset to the result
+            return;
         }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 
-        vector<int>v;
-        sort(nums.begin(),nums.end());
-        generate(0,nums,v);
-        return ans;
-        
+        v.push_back(nums[i]); // Include the current element in the subset
+        generate(i + 1, nums, v); // Recursively generate subsets with the current element
+
+        // Skip duplicates
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+            i++;
+        }
+
+        v.pop_back(); // Backtrack by removing the last added element
+        generate(i + 1, nums, v); // Recursively generate subsets without the current element
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> v; // Temporary vector to hold the current subset being built
+        sort(nums.begin(), nums.end()); // Sort the input vector to handle duplicates
+        generate(0, nums, v); // Start generating subsets from index 0
+        return ans; // Return the resulting subsets
     }
 };
