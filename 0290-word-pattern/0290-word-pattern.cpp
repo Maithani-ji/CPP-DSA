837e57aa8cc79df478d6609362aee71a7be0dc87
Tushar Maithani <maithaniji.official@gmail.com>
2023-09-15 23:37:51 +0530

    Time: 0 ms (100%), Space: 6.8 MB (11.06%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,47 +1,41 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <unordered_map>
 
 class Solution {
 public:
-    bool wordPattern(std::string pattern, std::string s) {
-        std::unordered_map<std::string, char> mp;
-        std::istringstream iss(s);
-        std::string word;
-        int i = 0;
-        int cn = 0;
+    bool wordPattern(string pattern, string s) {
+        // Initialize two unordered maps to store mappings between characters and words
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        
+        // Create a stringstream to tokenize the input string 's'
+        istringstream iss(s);
+        string word;
+        int i = 0; // Initialize the index for 'pattern'
 
         while (iss >> word) {
-            cn++;
-        }
-
-        if (cn != pattern.size()) {
-            return false;
-        }
-
-        // Reset the stringstream to its beginning
-        iss.clear();
-        iss.seekg(0);
-        int arr[26]={0};
-        while (iss >> word) {
-            if (mp.find(word) == mp.end()) {
-             if(arr[pattern[i]-'a']!=0)
-             {
-                 return false;
-             }
-             else{
-                mp[word] = pattern[i];
-                arr[pattern[i]-'a']=1;
-                i++;}
-            } else {
-                if (mp[word] != pattern[i]) {
-                    return false;
-                }
-                i++;
+            // If there are more words in 's' than characters in 'pattern', return false
+            if (i == pattern.size()) {
+                return false;
             }
+
+            char c = pattern[i]; // Get the current character from 'pattern'
+
+            // Check if the current character-to-word mapping exists and is consistent
+            if (charToWord.find(c) != charToWord.end() && charToWord[c] != word) {
+                return false;
+            }
+
+            // Check if the current word-to-character mapping exists and is consistent
+            if (wordToChar.find(word) != wordToChar.end() && wordToChar[word] != c) {
+                return false;
+            }
+
+            // Update the character-to-word and word-to-character mappings
+            charToWord[c] = word;
+            wordToChar[word] = c;
+            i++; // Move to the next character in 'pattern'
         }
 
-        return true; // Ensure both pattern and string are fully processed
+        // Ensure both 'pattern' and 's' are fully processed (i.e., no leftover characters or words)
+        return i == pattern.size();
     }
 };
