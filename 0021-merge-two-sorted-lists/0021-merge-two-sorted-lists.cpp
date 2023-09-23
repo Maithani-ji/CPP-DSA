252c6ee1f2004c98de013b9eec6b32dd42719cd6
Tushar Maithani <maithaniji.official@gmail.com>
2023-09-23 14:54:09 +0530

    Time: 3 ms (90.09%), Space: 15.1 MB (7.55%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,7 +11,7 @@
 
 // Create a structure definition for a singly-linked list node
 // It contains an integer value (val) and a pointer to the next node (next)
-
+/*
 class Solution {
 public:
     // Merge two sorted linked lists into one sorted linked list
@@ -40,5 +40,57 @@ public:
             l2->next = mergeTwoLists(l1, l2->next);
             return l2; // Return l2 as the new merged list
         }
+    }
+};*/
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       
+        // If list1 happens to be NULL, we will simply return list2.
+        if (list1 == NULL)
+            return list2;
+        
+        // If list2 happens to be NULL, we will simply return list1.
+        if (list2 == NULL)
+            return list1;
+        
+        ListNode *ptr = list1;
+        
+        // Compare the values of the first nodes in list1 and list2
+        if (list1->val > list2->val) {
+            // If the value in list2 is smaller, make ptr point to list2 and move list2 forward
+            ptr = list2;
+            list2 = list2->next;
+        } else {
+            // If the value in list1 is smaller or equal, move list1 forward
+            list1 = list1->next;
+        }
+        
+        ListNode *curr = ptr;
+        
+        // Until one of the lists reaches NULL
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                // If the value in list1 is smaller, connect curr to list1 and move list1 forward
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                // If the value in list2 is smaller or equal, connect curr to list2 and move list2 forward
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            // Move curr to the newly connected node
+            curr = curr->next;
+        }
+        
+        // Add the remaining elements of the non-empty list to the merged list
+        if (!list1)
+            curr->next = list2;
+        else
+            curr->next = list1;
+        
+        return ptr; // Return the merged list starting from ptr
+
     }
 };
