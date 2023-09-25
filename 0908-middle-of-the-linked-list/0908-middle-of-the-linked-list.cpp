ebcb072cef86be001be83016ffed9dc79b6aad01
Tushar Maithani <maithaniji.official@gmail.com>
2023-09-25 12:34:31 +0530

    Time: 0 ms (100%), Space: 7.4 MB (10.23%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -11,30 +11,51 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        int l=0;
-        ListNode *temp=head;
-        while(temp)
-        {
-            l++;
-            temp=temp->next;
+        // Move slow pointer one step and fast pointer two steps
+        // When fast pointer reaches the end, slow pointer will be at the middle
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-                // if(l%2==0)
-                // {
-                //     l=(l/2)+1;
-                // }
-                // else
-                // {
-                //     l=l/2;
-                // }
 
-        l=(l%2==0 )? (l/2) : l/2;
-        while(l)
-        {
-            head=head->next;
-            l--;
-        }
-        return head;
-
+        return slow;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int l = 0; // Initialize a variable to count the number of nodes in the list
+        ListNode* temp = head; // Create a temporary pointer to traverse the list
+        
+        // Calculate the length of the linked list
+        while (temp) {
+            l++;
+            temp = temp->next;
+        }
+
+        // Calculate the position of the middle node based on the length
+        l = l/2;// go to mid of length
+        // Traverse to the middle node
+        while (l) {
+            head = head->next;
+            l--;
+        }
+        
+        return head; // Return the middle node of the linked list
+    }
+};
+*/
