10ca2a5977346d3d2a8470c0ac2288588f7322a8
Tushar Maithani <maithaniji.official@gmail.com>
2023-09-28 22:59:02 +0530

    Time: 3 ms (91.11%), Space: 12.2 MB (9.89%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,42 +8,49 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution {//optimal soln  --> brute will be loop k times and always make the last first .
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-
-  if(!head || !head->next || k==0)
-        {
+        // Check for base cases:
+        // If the list is empty, has only one node, or k is 0, no rotation is needed.
+        if (!head || !head->next || k == 0) {
             return head;
         }
 
-        ListNode *temp=head;
-        int l=0;
-        while(temp)
-        {
-            if(temp->next==NULL)
-            {
-                temp->next=head;
-                l++;
+        // Initialize a temporary pointer to traverse the list and calculate its length.
+        ListNode* temp = head;
+        int length = 0;
+
+        // Traverse the list to find its length and make it circular by connecting the last node to the head.
+        while (temp) {
+            if (temp->next == NULL) {
+                temp->next = head; // Make it circular
+                length++; // Increment the length
                 break;
-            } 
-
-            l++;
-            temp=temp->next;
+            }
+            length++; // Increment the length
+            temp = temp->next; // Move to the next node
         }
-       
 
-             k=k%l;
-            k=l-k-1;
-            temp=head;
-           while(k--)
-           {
-                temp=temp->next;
-           }
-           head= temp->next;
-            temp->next=NULL;
-            return head;
+        // Calculate the effective rotation count by taking the remainder when k is divided by the length.
+        k = k % length;
 
-        
+        // Calculate the position from which to rotate by subtracting k from (length - 1).
+        k = length - 1-k;
+
+        // Reset the temporary pointer to the head.
+        temp = head;
+
+        // Move the temporary pointer to the node just before the new head after rotation.
+        while (k--) {
+            temp = temp->next;
+        }
+
+        // Update the 'head' pointer to the new head after rotation and break the circular structure.
+        head = temp->next;
+        temp->next = NULL;
+
+        // Return the updated head, which is the new head after rotation.
+        return head;
     }
 };
