3e7155bd71ae221ff1a0963c8426b878f92c8d7d
Tushar Maithani <maithaniji.official@gmail.com>
2023-10-06 14:52:01 +0530

    Time: 23 ms (92.4%), Space: 21.8 MB (47.12%) - LeetHub

--- 0835-linked-list-components/0835-linked-list-components.cpp
+++ 0835-linked-list-components/0835-linked-list-components.cpp
@@ -12,21 +12,25 @@ class Solution {
 public:
     // Function to count the number of components in a linked list
     int numComponents(ListNode* head, vector<int>& nums) {
+      
         // Create a set to store the values from the 'nums' vector for quick lookup
-        set<int> s;
-        for (auto i : nums) {
-            s.insert(i);
-        }
+      
+         unordered_set<int> s(nums.begin(), nums.end()); // Use unordered_set for O(1) lookup
+      
 
         bool flag = false; // A flag to indicate if we are inside a component
         int count = 0;     // Initialize the count of components to zero
 
         // Traverse the linked list
         while (head) {
+
             if (s.count(head->val) && !flag) {
+
                 flag = true; // Start of a new component
                 count++;     // Increment the component count
-            } else if (!s.count(head->val)) {
+            }
+             else if (!s.count(head->val))
+            {
                 flag = false; // End of the current component
             }
 
