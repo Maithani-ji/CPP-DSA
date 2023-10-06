4ab5bb6f03d4fe53f9c34ef6f071740f5e479701
Tushar Maithani <maithaniji.official@gmail.com>
2023-10-06 14:49:35 +0530

    Time: 33 ms (52.12%), Space: 22.3 MB (17.69%) - LeetHub

--- 0835-linked-list-components/0835-linked-list-components.cpp
+++ 0835-linked-list-components/0835-linked-list-components.cpp
@@ -10,31 +10,29 @@
  */
 class Solution {
 public:
+    // Function to count the number of components in a linked list
     int numComponents(ListNode* head, vector<int>& nums) {
-
-        set<int>s;
-        for(auto i: nums){
+        // Create a set to store the values from the 'nums' vector for quick lookup
+        set<int> s;
+        for (auto i : nums) {
             s.insert(i);
         }
 
-        bool flag=false;
-        int count=0;
+        bool flag = false; // A flag to indicate if we are inside a component
+        int count = 0;     // Initialize the count of components to zero
 
-        while(head)
-        {
-            if(s.count(head->val) and !flag)
-            {
-                flag=true;
-                count++;
-            }
-            else if(!s.count(head->val)) 
-            {
-                flag=false;
+        // Traverse the linked list
+        while (head) {
+            if (s.count(head->val) && !flag) {
+                flag = true; // Start of a new component
+                count++;     // Increment the component count
+            } else if (!s.count(head->val)) {
+                flag = false; // End of the current component
             }
 
-            head=head->next;
-
+            head = head->next; // Move to the next node in the linked list
         }
-        return count;
+
+        return count; // Return the total number of components
     }
 };
