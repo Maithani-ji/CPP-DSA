88114e905e3debc75e6c9c2923a9fa374d83278d
Tushar Maithani <maithaniji.official@gmail.com>
2023-10-08 16:57:16 +0530

    Time: 9 ms (37.24%), Space: 8.1 MB (20.79%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow, *fast;
+        slow = head; // Initialize the slow pointer to the head of the linked list
+        fast = head; // Initialize the fast pointer to the head of the linked list
+
+        while (fast && fast->next) {
+            slow = slow->next; // Move the slow pointer one step
+            fast = fast->next->next; // Move the fast pointer two steps
+
+            if (slow == fast) { // If they meet (a cycle is detected)
+                slow = head; // Reset the slow pointer to the head of the linked list
+
+                // Find the node where the cycle begins
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+
+                return slow; // Return the starting node of the cycle
+            }
+        }
+
+        return nullptr; // No cycle found in the linked list
+    }
+};
