2f31d1392726c3859f59ca40d17ba023afd8c859
Tushar Maithani <maithaniji.official@gmail.com>
2023-10-13 14:58:35 +0530

    Time: 0 ms (100%), Space: 6.2 MB (30.56%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int s = 1;                 // Initialize the start of the search range.
+        int e = n;                 // Initialize the end of the search range.
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;  // Calculate the middle number within the range.
+
+            long long k = static_cast<long long>(mid) * (mid + 1) / 2;  // Calculate the sum of the first 'mid' natural numbers.
+
+            if (k <= n) {
+                s = mid + 1;   // Adjust the start of the range if the sum is less than or equal to 'n'.
+            } else {
+                e = mid - 1;   // Adjust the end of the range if the sum is greater than 'n'.
+            }
+        }
+        return s - 1;  // Return 's - 1' as it represents the maximum number of full rows of coins that can be arranged within 'n' coins.
+    }
+};
+// or just use 
+// class Solution {// mThematical formula after deriving it from  k*(k+1)/2 <= n
+// public:
+//     int arrangeCoins(int n) {
+// return (int)(sqrt(2*long(n)+0.25)-0.5);
+// }
+// };
