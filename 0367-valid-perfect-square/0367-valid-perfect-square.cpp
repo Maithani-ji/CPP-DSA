a5256696a7f83326f7a5153e6b0791c96e81c0af
Tushar Maithani <maithaniji.official@gmail.com>
2023-10-13 14:16:44 +0530

    Time: 0 ms (100%), Space: 6.2 MB (30.4%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -3,9 +3,10 @@ public:
     bool isPerfectSquare(int num) {
         int s = 1;               // Initialize the start of the search range.
         int e = num / 2;         // Initialize the end of the search range.
-        if(num<=1) return true; // corner case 
-        while (s <=e) {
-            int64_t mid = s + (e - s) / 2;  // Calculate the middle number within the range.
+        if (num <= 1) return true; // Handle the corner case where num is 0 or 1, which are perfect squares.
+
+        while (s <= e) {
+            int64_t mid = s + (e - s) / 2;  // Calculate the middle number within the range. Use int64_t to avoid overflow.
 
             if (mid * mid > num) {
                 e = mid - 1;   // Adjust the end of the range if the square of 'mid' is too large.
