1ee238f62df94a11ea8f9fada778f1cc16213196
Tushar Maithani <maithaniji.official@gmail.com>
2023-10-16 16:26:46 +0530

    Time: 11 ms (82.98%), Space: 18.8 MB (41.89%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int hIndex(vector<int>& cit) {
-        int s=0;
-        int e=cit.size()-1;
-        int n=cit.size();
-        int mid;
+        int s = 0;               // Initialize the start of the search range.
+        int e = cit.size() - 1;  // Initialize the end of the search range.
+        int n = cit.size();      // Get the total number of papers in the 'cit' array.
+        int mid;                 // Initialize a variable to store the middle index.
 
-        while(s<=e)
-        {
-            mid=s+(e-s)/2;
+        while (s <= e) {         // Perform binary search within the range.
+            mid = s + (e - s) / 2;  // Calculate the middle index.
 
-            if(cit[mid]==(n-mid))
-            {
+            if (cit[mid] == (n - mid)) {
+                // If the number of citations at 'mid' equals the number of papers with more or equal citations,
+                // we have found the h-index, so we return it.
                 return cit[mid];
-            }
-            else if(cit[mid]>(n-mid))
-            {
-                e=mid-1;
-            }
-            else
-            {
-                s=mid+1;
+            } else if (cit[mid] > (n - mid)) {
+                // If the number of citations at 'mid' is greater than the number of papers with more or equal citations,
+                // adjust the ending index 'e' to search in the left half.
+                e = mid - 1;
+            } else {
+                // Otherwise, adjust the starting index 's' to search in the right half.
+                s = mid + 1;
             }
         }
-        return n-s;
+        
+        // If the loop exits and we haven't found the h-index, return 'n - s' as a fallback.
+        return n - s;
     }
 };
