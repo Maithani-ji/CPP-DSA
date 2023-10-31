476a3093460ef4cab86d1ef4e2331257127ccc10
Tushar Maithani <maithaniji.official@gmail.com>
2023-10-31 23:38:01 +0530

    Time: 0 ms (100%), Space: 6.7 MB (31.62%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int>seen;
-        while(n!=1)
-        { 
-            if(seen.count(n))
-            {
-                return false;
+        unordered_set<int> seen;  // To keep track of seen numbers and detect cycles.
+
+        while (n != 1) {
+            if (seen.count(n)) {
+                return false;  // If a number repeats, it's not a happy number.
             }
-            seen.insert(n);
-        int num=n;
-        int sum=0;
-            while(num)
-            {
-                int d=num%10;
-                sum+=d*d;
-                num=num/10;
+            seen.insert(n);  // Add the current number to the set.
 
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;  // Extract the last digit.
+                sum += digit * digit;  // Square and add the digit.
+                n /= 10;  // Remove the last digit.
             }
-            n=sum;
-
-
+            n = sum;  // Set n to the sum for the next iteration.
         }
-        return n==1?true:false;
-        
+
+        return true;  // If n eventually becomes 1, it's a happy number.
     }
 };
